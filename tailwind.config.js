f601a26309e5d4708e0d2f8772164ffba2cc298f
Chee Kian Teoh <teoh4770@mylaurier.ca>
2024-03-08 01:19:24 -0500

    💄 style: import font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+    content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
+    theme: {
+        fontFamily: {
+            sans: [
+                'Roboto',
+                'ui-sans-serif',
+                'system-ui',
+                'sans-serif',
+                'Apple Color Emoji',
+                'Segoe UI Emoji',
+                'Segoe UI Symbol',
+                'Noto Color Emoji',
+            ],
+        },
+        extend: {},
+    },
+    plugins: [],
+};
