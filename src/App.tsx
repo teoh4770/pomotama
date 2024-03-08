10dc6ba4c80c1f7fc10ff1fbc421177d1d4a627c
Chee Kian Teoh <teoh4770@mylaurier.ca>
2024-03-08 00:05:56 -0500

    ✨feat: Timer component functionality complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { Timer } from './components';
+
 function App() {
     return (
         <>
             <p className="text-3xl font-bold underline">complete the setup</p>
+            <Timer />
         </>
     );
 }
---
+++ src/components/Timer/Timer.tsx
@@ -0,0 +1,94 @@
+/* API Design
+`Timer`: the main component that contains:
+  - Tabs
+  - Time
+  - Controls
+
+States
+- initialTime: the initial amount of time
+- timeElapsed: keep track of the amount of time passed
+- status: to run or stop the interval based on status
+
+
+Effect
+- setInterval
+
+Handlers
+- toggle the timer
+- reset the timer
+ */
+
+import { useState } from 'react';
+import { Controls } from './Controls';
+import { Tabs } from './Tabs';
+import { Time } from './Time';
+import { useInterval } from '../../effects';
+import { minutesToSeconds, getTimes } from '../../utils';
+
+const timerSettings = [
+    {
+        name: 'pomodoro',
+        label: 'Pomodoro',
+        value: 15,
+    },
+    {
+        name: 'shortBreak',
+        label: 'Short Break',
+        value: 5,
+    },
+    {
+        name: 'longBreak',
+        label: 'Long Break',
+        value: 20,
+    },
+];
+
+// contain all the timer related logic
+const Timer = () => {
+    const [initialTime, setInitialTime] = useState(0);
+    const [timeElapsed, setTimeElapsed] = useState(0);
+    const [status, setStatus] = useState('idle'); // idle or running
+
+    const time = getTimes(initialTime - timeElapsed);
+
+    useInterval(
+        () => {
+            setTimeElapsed((timeElapsed) => timeElapsed + 1);
+        },
+        status === 'running' ? 1000 : null
+    );
+
+    const updateTimer = (val: number) => {
+        if (val < 0) throw new Error('Cannot set negative value for timer');
+        setStatus('idle');
+        setInitialTime(minutesToSeconds(val));
+    };
+
+    const toggleTimer = () => {
+        setStatus((status) => {
+            if (status === 'running') {
+                return 'idle';
+            }
+            return 'running';
+        });
+    };
+
+    const resetTimer = () => {
+        setStatus('idle');
+        setTimeElapsed(0);
+    };
+
+    return (
+        <article className="timer">
+            <Tabs items={timerSettings} handler={updateTimer} />
+            <Time minutes={time.minutes} seconds={time.seconds} />
+            <Controls
+                status={status}
+                toggleTimer={toggleTimer}
+                resetTimer={resetTimer}
+            />
+        </article>
+    );
+};
+
+export { Timer };
