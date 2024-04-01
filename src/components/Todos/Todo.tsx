b6cd51d24fcb23cb8865998d4f5d3cce9e00dad6
Chee Kian Teoh <teoh4770@mylaurier.ca>
2024-04-01 00:07:09 -0400

    feat: enabled editTodo and removeTodo

--- src/components/Todos/Todo.tsx
+++ src/components/Todos/Todo.tsx
@@ -1,28 +1,110 @@
 import { Todo } from '../../lib/atom';
+import { FormEvent, useState } from 'react';
+
+interface TodoFormData {
+    title: string;
+}
 
 interface TodoProps {
     todo: Todo;
     toggleTodoState: (id: string) => void;
+    editTodo: (id: string, formData: TodoFormData) => void;
+    removeTodo: (id: string) => void;
 }
 
-const TodoItem = ({ todo, toggleTodoState }: TodoProps) => {
+const TodoItem = ({
+    todo,
+    toggleTodoState,
+    editTodo,
+    removeTodo,
+}: TodoProps) => {
+    const [editing, setEditing] = useState(false);
+    // handle delete
+    // handle save/edit
+    // handle cancel editing
+    // can handle it outside (pass a callback in)
+    // can pass an object in
+    const saveTodo = (e: FormEvent) => {
+        e.preventDefault();
+
+        const $form = e.currentTarget as HTMLFormElement;
+        const formData = Object.fromEntries(new FormData($form));
+
+        const todoFormData: TodoFormData = {
+            title: formData.title as string,
+        };
+        editTodo(todo.id, todoFormData);
+
+        setEditing(false);
+    };
+
     return (
-        <li key={todo.id} className="flex items-center border p-2">
-            <label className="mr-auto">
-                <input
-                    id={todo.id}
-                    type="checkbox"
-                    name="todo1"
-                    checked={todo.completed}
-                    onChange={() => toggleTodoState(todo.id)}
-                />
-                {todo.title}
-            </label>
-            <button className="button" data-type="secondary" data-size="small">
-                Options
-            </button>
+        <li className="border p-2">
+            {editing ? (
+                <form
+                    onSubmit={saveTodo}
+                    id="task-adder"
+                    className="task-editor grid [&>*]:border [&>*]:p-2"
+                >
+                    <label>
+                        <span className="sr-only">New Todo</span>
+                        <input
+                            type="text"
+                            name="title"
+                            className="w-full border bg-transparent pl-1"
+                            required
+                            defaultValue={todo.title}
+                        />
+                    </label>
+
+                    <div className="todo-options flex justify-between">
+                        <button
+                            type="button"
+                            className="button"
+                            data-type="naked"
+                            onClick={() => removeTodo(todo.id)}
+                        >
+                            delete
+                        </button>
+                        <div className="flex">
+                            <button
+                                type="button"
+                                className="button"
+                                data-type="naked"
+                                onClick={() => setEditing(false)}
+                            >
+                                Cancel
+                            </button>
+                            <button type="submit" data-type="confirm">
+                                Save
+                            </button>
+                        </div>
+                    </div>
+                </form>
+            ) : (
+                <div id={todo.id} className="todo-item flex items-center">
+                    <label className="mr-auto">
+                        <input
+                            id={todo.id}
+                            type="checkbox"
+                            name="todo1"
+                            checked={todo.completed}
+                            onChange={() => toggleTodoState(todo.id)}
+                        />
+                        {todo.title}
+                    </label>
+                    <button
+                        className="button"
+                        data-type="secondary"
+                        data-size="small"
+                        onClick={() => setEditing(true)}
+                    >
+                        Options
+                    </button>
+                </div>
+            )}
         </li>
     );
 };
 
-export { TodoItem }
+export { TodoItem };
--- src/components/Todos/Todos.tsx
+++ src/components/Todos/Todos.tsx
@@ -1,6 +1,6 @@
 import { useAtom } from 'jotai';
 import { Todo, todosAtom } from '../../lib/atom';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { updateItemsInLocalStorage } from '../../localStorage';
 import { Form } from './Form';
 import { TodoItem } from './Todo';
@@ -26,7 +26,28 @@ const Todos = () => {
         });
     };
 
-    const toggleTodoCompletedState = (id: string) => {
+    // I don't want every todo have this function, i wanna pass it down instead
+    // formData interface
+    const editTodo = (id: string, formData: { title: string }) => {
+        const todo = todos.find((todo) => todo.id === id) as Todo;
+        const updatedTodo = { ...todo, ...formData };
+        const updatedTodos = todos.map((todo) => {
+            if (todo.id === id) {
+                return updatedTodo;
+            }
+            return todo;
+        });
+
+        setTodos(updatedTodos);
+    };
+
+    const removeTodo = (id: string) => {
+        setTodos((prevTodos: Todo[]) => {
+            return prevTodos.filter((todo) => todo.id !== id);
+        });
+    };
+
+    const toggleTodoState = (id: string) => {
         setTodos((prevTodos: Todo[]) => {
             const updatedTodos = prevTodos.map((todo) => {
                 if (todo.id === id) {
@@ -43,24 +64,20 @@ const Todos = () => {
 
     return (
         <section className="tasks-section">
-            {/* tasks header */}
             <Header />
-            {/* tasks todo list */}
             <ol id="todo-list" className="todo-list mb-3 mt-5 grid gap-2">
                 {todos.map((todo) => (
                     <TodoItem
                         key={todo.id}
                         todo={todo}
-                        toggleTodoState={toggleTodoCompletedState}
+                        toggleTodoState={toggleTodoState}
+                        editTodo={editTodo}
+                        removeTodo={removeTodo}
                     />
                 ))}
             </ol>
 
-            {/* form component, 2 states
-              -> expanded form when add button is clicked
-              -> expanded form when editing the todo
-            */}
-            <Form mode={'editTodo'} onSubmit={addTodo} />
+            <Form mode={'addTodo'} onSubmit={addTodo} />
         </section>
     );
 };
