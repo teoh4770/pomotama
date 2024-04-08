3ce90b27c8a162f56a65970e65e8a4a08d514cb4
Chee Kian Teoh <teoh4770@mylaurier.ca>
2024-04-08 15:30:22 -0400

    add: instructions to use the pomodoro timer

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,22 @@ const App = () => {
             </Header>
             <Timer />
             <Todos />
+            <div className="mt-10 bg-white">
+                <h2 className="text-xl font-bold">
+                    How to use the Pomodoro Timer?
+                </h2>
+                <ol className="list-decimal ml-10">
+                    <li>Add tasks to work on today</li>
+                    <li>
+                        Set estimate pomodoros (1 = 25min of work) for each
+                        tasks
+                    </li>
+                    <li>Select a task to work on</li>
+                    <li>Start timer and focus on the task for 25 minutes</li>
+                    <li>Take a break for 5 minutes when the alarm ring</li>
+                    <li>Iterate 3-5 until you finish the tasks</li>
+                </ol>
+            </div>
         </main>
     );
 };
