0722f31a456c018cf574883647d04b23c9c18d91
Chee Kian Teoh <teoh4770@mylaurier.ca>
2024-04-17 01:03:57 -0400

    style: instruction section

--- src/App.tsx
+++ src/App.tsx
@@ -3,48 +3,51 @@ import { Setting, Timer, Todos } from './components';
 
 const App = () => {
     return (
-        <main className="px-3 sm:px-4">
-            <header className="mx-auto flex max-w-2xl items-center gap-2 py-4">
-                <h1 className="mr-auto text-2xl font-bold text-white ">
-                    Pomotama
-                </h1>
-
-                <button
-                    type="button"
-                    className="button"
-                    data-type="secondary"
-                    data-size="small"
-                    aria-label="tutorial button"
-                    onClick={runTour}
-                >
-                    Tutorial
-                </button>
-                <Setting />
-            </header>
-            <Timer />
-            <Todos />
-            <div
-                id="instruction"
-                className="my-10 rounded-lg bg-white p-6 shadow"
-            >
-                <h2 className="text-xl font-bold text-gray-900">
-                    How to use the Pomodoro Timer?
-                </h2>
-                <ol className="prose mt-4 list-decimal space-y-2 pl-5 text-gray-700">
-                    <li>Add tasks to work on today</li>
-                    <li>
-                        Set estimate pomodoros (1 = 25min of work) for each task
-                    </li>
-                    <li>Select a task to work on</li>
-                    <li>Start timer and focus on the task for 25 minutes</li>
-                    <li>Take a break for 5 minutes when the alarm rings</li>
-                    <li>Iterate 3-5 until you finish the tasks</li>
-                </ol>
-                <p className="mt-4 text-sm text-gray-600">
-                    💡 Tip: The selected task will update its est.pomodoro
-                    number once the pomodoro timer has finished!
-                </p>
-            </div>
+        <main>
+            <section aria-label="app" className="px-3 sm:px-4">
+                <header className="mx-auto flex max-w-2xl items-center gap-2 py-4">
+                    <h1 className="mr-auto text-2xl font-bold text-white ">
+                        Pomotama
+                    </h1>
+                    <button
+                        type="button"
+                        className="button"
+                        data-type="secondary"
+                        data-size="small"
+                        aria-label="tutorial button"
+                        onClick={runTour}
+                    >
+                        Tutorial
+                    </button>
+                    <Setting />
+                </header>
+                <Timer />
+                <Todos />
+            </section>
+            <section aria-label="description" className="mt-10 bg-white/85">
+                <div id="instruction" className="mx-auto max-w-2xl p-6">
+                    <h2 className="text-xl font-bold text-gray-900">
+                        How to use the Pomodoro Timer?
+                    </h2>
+                    <ol className="prose mt-4 list-decimal space-y-2 pl-5 text-gray-700">
+                        <li>Add tasks to work on today</li>
+                        <li>
+                            Set estimate pomodoros (1 = 25min of work) for each
+                            task
+                        </li>
+                        <li>Select a task to work on</li>
+                        <li>
+                            Start timer and focus on the task for 25 minutes
+                        </li>
+                        <li>Take a break for 5 minutes when the alarm rings</li>
+                        <li>Iterate 3-5 until you finish the tasks</li>
+                    </ol>
+                    <p className="mt-4 text-sm text-gray-600">
+                        💡 Tip: The selected task will update its est.pomodoro
+                        number once the pomodoro timer has finished!
+                    </p>
+                </div>
+            </section>
         </main>
     );
 };
