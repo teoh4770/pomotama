01b817e4f61a8a29432aea2981156243643d48eb
Chee Kian Teoh <teoh4770@mylaurier.ca>
2024-04-30 15:42:04 -0400

    :bug:fix: update the Timer Tabs in Timer

--- src/components/Timer/Timer.section.tsx
+++ src/components/Timer/Timer.section.tsx
@@ -2,7 +2,7 @@ import { useAtomValue } from 'jotai';
 import { timerSettingsAtom } from '../../lib/atom';
 
 import { Time, Indicator, TimerControls } from '.';
-import { Tabs } from '../Tabs';
+import { TimerTabs } from './TimerTabs';
 
 import { TimerModeEnum, TimerStatusEnum } from '../../types';
 
@@ -35,28 +35,41 @@ const Timer = ({ timer }: TimerProps) => {
             />
 
             <article className="timer mx-auto max-w-[30rem] space-y-6 rounded-lg bg-white/10 px-4 py-6 text-center text-white sm:space-y-8 sm:py-8">
-                <Tabs
+                <TimerTabs
                     items={[
                         {
                             name: TimerModeEnum.POMODORO,
                             label: 'Pomodoro',
                             value: timerSettings.pomodoroDuration,
+                            handleClick: () =>
+                                timer.actions.changeTimerMode(
+                                    TimerModeEnum.POMODORO
+                                ),
                         },
                         {
                             name: TimerModeEnum.SHORT_BREAK,
                             label: 'Short Break',
                             value: timerSettings.shortBreakDuration,
+                            handleClick: () =>
+                                timer.actions.changeTimerMode(
+                                    TimerModeEnum.SHORT_BREAK
+                                ),
                         },
                         {
                             name: TimerModeEnum.LONG_BREAK,
                             label: 'Long Break',
                             value: timerSettings.longBreakDuration,
+                            handleClick: () =>
+                                timer.actions.changeTimerMode(
+                                    TimerModeEnum.LONG_BREAK
+                                ),
                         },
                     ]}
-                    handler={timer.actions.changeTimerMode}
                     timerMode={timer.timerMode}
+                    tabListClassName="center | flex w-fit flex-row"
+                    tabItemClassName="bg-transparent"
                 />
-                
+
                 <Time remainingTime={timer.remainingTime} />
 
                 <TimerControls
