f5654bb555e3b0d575197891046f5e38402b1910
Chee Kian Teoh <teoh4770@mylaurier.ca>
2024-08-20 22:31:21 -0400

    ✅test: time util functions

--- src/utils/__test__/time.spec.ts
+++ src/utils/__test__/time.spec.ts
@@ -1,120 +1,113 @@
-import { getTimes, formatTime, clipTime, addMinutesToTime } from '../time';
+import { getTimes, formatTime, addMinutesToTime } from '../time';
 
-describe('getTimes', () => {
-    it('should return minutes and seconds for a given time in seconds', () => {
-        const timeInSeconds = 123;
-        const expectedTime = { minutes: 2, seconds: 3 };
-
-        const time = getTimes(timeInSeconds);
-
-        expect(time).toEqual(expectedTime);
+describe('getTimes()', () => {
+    test('returns an object', () => {
+        expect(typeof getTimes(60)).toBe('object');
+        expect(typeof getTimes(-60)).toBe('object');
     });
 
-    it('should return 0 minutes and seconds for negative time in seconds', () => {
-        const timeInSeconds = -1;
-        const expectedTime = { minutes: 0, seconds: 0 };
-
-        const time = getTimes(timeInSeconds);
-
-        expect(time).toEqual(expectedTime);
-    });
-});
-
-describe('formattedTimes', () => {
-    it('should return the same value if the time is larger or equal to 10', () => {
-        const time = 11;
-        const expectedTime = '11';
-
-        const formattedTime = formatTime(time);
-
-        expect(formattedTime).toBe(expectedTime);
+    test('positive number cases', () => {
+        expect(getTimes(+0)).toEqual({ minutes: 0, seconds: 0 });
+        expect(getTimes(1)).toEqual({ minutes: 0, seconds: 1 });
+        expect(getTimes(123)).toEqual({ minutes: 2, seconds: 3 });
+        expect(getTimes(180)).toEqual({ minutes: 3, seconds: 0 });
+        expect(getTimes(1833)).toEqual({ minutes: 30, seconds: 33 });
     });
 
-    it('should prepend a zero to the time if it is between 0 and 10', () => {
-        const time = 0;
-        const expectedTime = '00';
+    test('negative number cases', () => {
+        const defaultTime = { minutes: 0, seconds: 0 };
 
-        const formattedTime = formatTime(time);
-
-        expect(formattedTime).toBe(expectedTime);
-    });
-
-    it('should throw an error if input is negative', () => {
-        const time = -1;
-
-        expect(() => formatTime(time)).toThrow();
+        expect(getTimes(-0)).toEqual(defaultTime);
+        expect(getTimes(-1)).toEqual(defaultTime);
+        expect(getTimes(-2.5)).toEqual(defaultTime);
     });
 });
 
-describe('clipTime', () => {
-    const MIN = 0;
-    const MAX = 999;
-
-    it('should return the same time if it is between 0 and 999', () => {
-        const time = 1;
-        const expectedTime = 1;
-
-        const result = clipTime(time);
-
-        expect(result).toBe(expectedTime);
+describe('formattedTimes()', () => {
+    test('returns a string', () => {
+        expect(typeof formatTime(0)).toBe('string');
+        expect(typeof formatTime(60)).toBe('string');
     });
 
-    it(`should return ${MIN} if the time is negative`, () => {
-        const time = -1;
-        const expectedTime = MIN;
-
-        const result = clipTime(time);
-        expect(result).toBe(expectedTime);
+    test('single digit', () => {
+        expect(formatTime(0)).toBe('00');
+        expect(formatTime(1)).toBe('01');
+        expect(formatTime(5)).toBe('05');
     });
 
-    it(`should return ${MAX} if the time is greater than ${MAX}`, () => {
-        const time = 1000;
-        const expectedTime = MAX;
+    test('double digit', () => {
+        expect(formatTime(11)).toBe('11');
+        expect(formatTime(50)).toBe('50');
+        expect(formatTime(60)).toBe('60');
+    });
 
-        const result = clipTime(time);
-        expect(result).toBe(expectedTime);
+    test('triple digit', () => {
+        expect(formatTime(110)).toBe('110');
+        expect(formatTime(500)).toBe('500');
+        expect(formatTime(600)).toBe('600');
+    });
+
+    test('negative number cases', () => {
+        expect(() => formatTime(-60)).toThrow();
     });
 });
 
-describe('addMinutesToTime', () => {
-    it('should add additional minutes to the given time', () => {
-        const baseTime = { hours: 21, minutes: 30 };
-        const additionalMinutes = 30;
-
-        const result = addMinutesToTime(baseTime, additionalMinutes);
-        expect(result).toStrictEqual({ hours: 22, minutes: 0 });
-    });
-
-    it('should wrap hours within 0 to 23 range when exceeding 24 hours', () => {
-        const baseTime = { hours: 23, minutes: 30 };
-        const additionalMinutes = 60;
-
-        const result = addMinutesToTime(baseTime, additionalMinutes);
-        expect(result).toStrictEqual({ hours: 0, minutes: 30 });
-    });
-
-    it('should handle large amounts of additional minutes and wrap hours correctly', () => {
-        const baseTime = { hours: 23, minutes: 30 };
-        const additionalMinutes = 1500; // 25 hours (1500 minutes)
-
-        const result = addMinutesToTime(baseTime, additionalMinutes);
-        expect(result).toEqual({ hours: 0, minutes: 30 });
-    });
-
-    it('should return the base time if additionalMinutes are 0', () => {
-        const baseTime = { hours: 21, minutes: 30 };
-        const additionalMinutes = 0;
-
-        const result = addMinutesToTime(baseTime, additionalMinutes);
-        expect(result).toStrictEqual(baseTime);
-    });
-
-    it('should throw an error if additionalMinutes input is negative', () => {
-        const baseTime = { hours: 23, minutes: 30 };
-        const additionalMinutes = -60;
-
-        expect(() => addMinutesToTime(baseTime, additionalMinutes)).toThrow(
-            'additionalMinutes cannot accept negative values...'
+describe('addMinutesToTime()', () => {
+    test('returns an object', () => {
+        expect(typeof addMinutesToTime({ hours: 1, minutes: 1 }, 10)).toBe(
+            'object'
         );
+        expect(typeof addMinutesToTime({ hours: 10, minutes: 5 }, 30)).toBe(
+            'object'
+        );
+    });
+
+    test('negative additional minutes cases', () => {
+        const baseTime = { hours: 21, minutes: 30 };
+
+        expect(() => addMinutesToTime(baseTime, -1)).toThrow();
+        expect(() => addMinutesToTime(baseTime, -10)).toThrow();
+    });
+
+    test('negative time object cases', () => {
+        expect(() =>
+            addMinutesToTime({ hours: -21, minutes: -30 }, 10)
+        ).toThrow();
+        expect(() =>
+            addMinutesToTime({ hours: -1, minutes: -3 }, 10)
+        ).toThrow();
+    });
+
+    describe('Given the current time is 1 hour 30 minutes', () => {
+        const currentTime = { hours: 1, minutes: 30 };
+
+        test('When adding an additional 30 minutes', () => {
+            const additionalMinutes = 30;
+            const time = addMinutesToTime(currentTime, additionalMinutes);
+
+            expect(time).toStrictEqual({ hours: 2, minutes: 0 });
+        });
+    });
+
+    describe('Given the current time is 23 hour 30 minutes', () => {
+        const currentTime = { hours: 23, minutes: 30 };
+
+        test('When adding an additional 30 minutes', () => {
+            const additionalMinutes = 30;
+            const time = addMinutesToTime(currentTime, additionalMinutes);
+
+            expect(time).toStrictEqual({ hours: 0, minutes: 0 });
+        });
+    });
+
+    describe('Given the current time is 23 hour 30 minutes', () => {
+        const currentTime = { hours: 23, minutes: 30 };
+
+        test('When adding an additional 1500 minutes', () => {
+            const additionalMinutes = 1500; // 25 hours
+            const time = addMinutesToTime(currentTime, additionalMinutes);
+
+            expect(time).toStrictEqual({ hours: 0, minutes: 30 });
+        });
     });
 });
